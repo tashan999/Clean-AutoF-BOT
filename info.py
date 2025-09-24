09d6798d9131e8e21f0b1b43cb716bcc29bf9d6e
tashan999 <138196222+tashan999@users.noreply.github.com>
2025-09-24 14:08:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "akshat")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tzcollection')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tzcollection2')
 
 # ============================
 # Movie Notification & Update Settings
